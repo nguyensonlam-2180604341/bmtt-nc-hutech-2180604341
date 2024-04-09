e3361909ed72e4fd466bb4bc333ca1b0366a9bd7
NguyenSonLam_2180604341 <badonjohn4@gmail.com>
2024-04-09 09:08:28 +0700

    [add] lab-02 railfence #2

--- lab-02/api.py
+++ lab-02/api.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from cipher.caesar.caesar_cipher import CaesarCipher
 from cipher.vigenere.vigenere_cipher import VigenereCipher
+from cipher.railfence.railfence_cipher import RailFenceCipher
 
 app = Flask (__name__)
 
@@ -12,7 +13,6 @@ def caesar_encrypt():
     key= int(data['key'])
     encrypted_text = caesar_cipher. encrypt_text (plain_text, key)
     return jsonify({'encrypted message': encrypted_text})
-
 @app.route("/api/caesar/decrypt", methods=["POST"])
 def caesar_decrypt():
     data = request.json
@@ -22,7 +22,6 @@ def caesar_decrypt():
     return jsonify ({'decrypted_message': decrypted_text})
 
 vigenere_cipher = VigenereCipher ()
-
 @app.route('/api/vigenere/encrypt', methods=['POST'])
 def vigenere_encrypt():
     data=request.json
@@ -30,7 +29,6 @@ def vigenere_encrypt():
     key=data['key']
     encrypted_text=vigenere_cipher.vigenere_encrypt (plain_text, key)
     return jsonify ({'encrypted_text': encrypted_text})
-
 @app.route('/api/vigenere/decrypt', methods = ['POST'])
 def vigenere_decrypt ():
     data=request.json
@@ -38,6 +36,22 @@ def vigenere_decrypt ():
     key=data['key']
     decrypted_text = vigenere_cipher.vigenere_decrypt (cipher_text, key)
     return jsonify ({'decrypted_text': decrypted_text})
+
+railfence_cipher = RailFenceCipher ()
+@app.route("/api/railfence/encrypt", methods=["POST"])
+def encrypt():
+    data=request.json
+    plain_text=data['plain_text']
+    key= int(data['key'])
+    encrypted_text = railfence_cipher.rail_fence_encrypt (plain_text, key)
+    return jsonify({'encrypted message': encrypted_text})
+@app.route("/api/railfence/decrypt", methods=["POST"])
+def decrypt():
+    data = request.json
+    cipher_text = data['cipher_text']
+    key = int (data['key'])
+    decrypted_text = railfence_cipher.rail_fence_decrypt (cipher_text, key)
+    return jsonify ({'decrypted_message': decrypted_text})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
